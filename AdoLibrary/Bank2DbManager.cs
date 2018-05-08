f3cf8db6934a4bc52b73c17e529b72b75118fa80
Swouterke <wvercruysse45@gmail.com>
2018-05-08 15:03:40 +0200

    Add project files.

---
+++ AdoLibrary/Bank2DbManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.Common;
+
+namespace AdoLibrary
+{
+    public class Bank2DbManager
+    {
+        private static ConnectionStringSettings conBankSetting = ConfigurationManager.ConnectionStrings["Bank2"];
+        private static DbProviderFactory factory = DbProviderFactories.GetFactory(conBankSetting.ProviderName);
+
+        public DbConnection GetConnection()
+        {
+            var conBank = factory.CreateConnection();
+            conBank.ConnectionString = conBankSetting.ConnectionString;
+            return conBank;
+        }
+    }
+}
